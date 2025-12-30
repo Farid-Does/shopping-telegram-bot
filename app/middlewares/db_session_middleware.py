502c9fbeda82c7975e1ed76b1da17c5cb537b132
Farid Alizadeh <fariddoes@gmail.com>
2025-12-30 09:21:47 -0800

    add db session middleware.

--- app/middlewares/db_session_middleware.py
+++ app/middlewares/db_session_middleware.py
@@ -1,2 +1,20 @@
 # session middleware to inject sessions into handlers
 
+
+from aiogram import BaseMiddleware
+from db.database import async_session
+
+
+# create db session middleware
+class DbSessionMiddleware(BaseMiddleware):
+    async def __call__(self, handler, event, data: dict):
+        async with async_session() as session:
+            data["db"] = session
+
+        try:
+            result = await handler(event)
+            await session.commit()
+            return result
+        except Exception:
+            await session.rollback()
+            raise
