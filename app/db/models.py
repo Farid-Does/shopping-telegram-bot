aa2099fb124c471e9dd5eefa4298c7003cc1e182
Farid Alizadeh <fariddoes@gmail.com>
2025-12-31 07:27:07 -0800

    changes on the models and start the handlers.

--- app/db/models.py
+++ app/db/models.py
@@ -1,11 +1,13 @@
 # creating models
 
 
-from sqlalchemy import Table, Column, ForeignKey, Integer, String, Text, DateTime, Boolean, Numeric, DECIMAL, UniqueConstraint
+from sqlalchemy import Table, Column, ForeignKey, Integer, String, Text, DateTime, Boolean, Numeric, DECIMAL, UniqueConstraint, BIGINT
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 from sqlalchemy.ext.hybrid import hybrid_property
-
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 
 
 
@@ -37,15 +39,16 @@ cart_product = Table(
     Column("quantity", Integer, nullable=False, default=1)
 )
 
-# Order ↔ Product (OrderItems)
-order_items_table = Table(
-    "order_items_assoc",
+# Order ↔ Product (association table)
+order_items = Table(
+    "order_items",
     Base.metadata,
     Column("order_id", ForeignKey("orders.id"), primary_key=True),
     Column("product_id", ForeignKey("products.id"), primary_key=True),
     Column("quantity", Integer, nullable=False, default=1),
-    Column("price_at_purchase", Numeric(10,2), nullable=False)
+    Column("price_at_purchase", Numeric(10, 2), nullable=False),
 )
+
 
 # ------------------------------
 # models
@@ -55,7 +58,7 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    telegram_id: Mapped[int] = mapped_column(unique=True, nullable=False)
+    telegram_id: Mapped[int] = mapped_column(BIGINT, unique=True, nullable=False)
     first_name: Mapped[str | None]
     last_name: Mapped[str | None]
     username: Mapped[str] = mapped_column(unique=True, nullable=False)
@@ -76,6 +79,33 @@ class User(Base):
     @property
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
+    
+    @classmethod
+    async def get_user_by_telegram_id(cls, db: AsyncSession, telegram_id: int):
+        result = await db.execute(select(cls).where(cls.telegram_id == telegram_id))
+        return result.scalar_one_or_none()
+    
+    @classmethod
+    async def create_user(
+        cls,
+        db: AsyncSession,
+        telegram_id: int,
+        language_code: str,
+        username: str,
+        first_name: str | None = None,
+        last_name: str | None = None
+    ):
+        new_user = cls(
+            telegram_id=telegram_id,
+            language_code=language_code,
+            username=username,
+            first_name=first_name,
+            last_name=last_name
+        )
+        db.add(new_user)
+        
+        return new_user
+
 
 
 class Category(Base):
@@ -105,8 +135,8 @@ class Product(Base):
 
     categories: Mapped[list["Category"]] = relationship(secondary=product_category, back_populates="products")
     images: Mapped[list["Image"]] = relationship(back_populates="product")
-    orders: Mapped[list["OrderItems"]] = relationship(secondary=order_items_table, back_populates="products")
-    carts: Mapped[list["Cart"]] = relationship(secondary=cart_product, back_populates="products")
+    order_items: Mapped[list["Order"]] = relationship(secondary=order_items, back_populates="products")
+    cart_items: Mapped[list["Cart"]] = relationship(secondary=cart_product, back_populates="products")
 
 
 class Image(Base):
@@ -140,7 +170,7 @@ class Cart(Base):
     created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now())
 
     user: Mapped["User"] = relationship(back_populates="carts")
-    products: Mapped[list["Product"]] = relationship(secondary=cart_product, back_populates="carts")
+    products: Mapped[list["Product"]] = relationship(secondary=cart_product, back_populates="cart_items")
 
 
 class Order(Base):
@@ -156,19 +186,9 @@ class Order(Base):
     user: Mapped["User"] = relationship(back_populates="orders")
     shipping: Mapped["OrderShipping"] = relationship(back_populates="order", uselist=False)
     payment: Mapped["Payment"] = relationship(back_populates="order", uselist=False)
-    products: Mapped[list["Product"]] = relationship(secondary=order_items_table, back_populates="orders")
+    products: Mapped[list["Product"]] = relationship(secondary=order_items, back_populates="order_items")
 
 
-class OrderItems(Base):
-    __tablename__ = "order_items"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    order_id: Mapped[int] = mapped_column(ForeignKey("orders.id"), nullable=False)
-    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), nullable=False)
-    price_at_purchase: Mapped[Numeric] = mapped_column(Numeric(10,2), nullable=False)
-    quantity: Mapped[int] = mapped_column(nullable=False)
-
-    products: Mapped[list["Product"]] = relationship(secondary=order_items_table, back_populates="orders")
 
 
 class OrderShipping(Base):
